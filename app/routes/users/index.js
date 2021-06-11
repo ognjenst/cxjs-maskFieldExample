74572041b220a59f936d3e10d44945bfbb06a81d
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2021-06-11 23:05:06 +0200

    Prettier

--- app/routes/users/index.js
+++ app/routes/users/index.js
@@ -1,15 +1,15 @@
-import { Route } from "cx/widgets";
+import { Route } from 'cx/widgets';
 
-import List from "./List";
-import Editor from "./Editor";
+import List from './List';
+import Editor from './Editor';
 
 export default (
-    <cx>
-        <Route route="~/users" url-bind="url">
-            <List />
-        </Route>
-        <Route route="~/users/:userId" url-bind="url">
-            <Editor />
-        </Route>
-    </cx>
+	<cx>
+		<Route route='~/users' url-bind='url'>
+			<List />
+		</Route>
+		<Route route='~/users/:userId' url-bind='url'>
+			<Editor />
+		</Route>
+	</cx>
 );
